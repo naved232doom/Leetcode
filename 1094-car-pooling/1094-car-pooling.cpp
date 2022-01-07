8587ca6850ffbb83beacf88d4515b7f38e9f2908
Naved Sohail <65990919+naved232doom@users.noreply.github.com>
2022-01-07 11:41:18 +0530

    Time: 21 ms (25.50%), Space: 9.8 MB (91.17%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-           int n=(int)(trips.size());
-            int maxn=0;
-            for(int i=0;i<n;++i){
-                    maxn=max(maxn,trips[i][2]);
+        int n = (int)(trips.size());
+        int maxn = 0;
+        for (int i = 0; i < n; ++i) {
+            maxn = max(maxn, trips[i][2]);
+        }
+        vector<int> pref(maxn + 1);
+        for (int i = 0; i < n; ++i) {
+            pref[trips[i][1]] += trips[i][0];
+            pref[trips[i][2]] -= trips[i][0];
+        }
+        bool ok = 1;
+        for (int i = 0; i < maxn + 1; ++i) {
+            if (i - 1 >= 0)
+                pref[i] += pref[i - 1];
+            if (pref[i] > capacity) {
+                ok = 0;
+                break;
             }
-        vector<int> pref(maxn+1);
-            for(int i=0;i<n;++i){
-                    pref[trips[i][1]]+=trips[i][0];
-                    pref[trips[i][2]]-=trips[i][0];
-            }
-            bool ok=1;
-            for(int i=0;i<maxn+1;++i){
-                    if(i-1>=0)
-                    pref[i]+=pref[i-1];
-                    if(pref[i]>capacity){
-                            ok=0;
-                            break;
-                    }
-            }
-            
-            return ok;
+        }
+
+        return ok;
     }
 };
