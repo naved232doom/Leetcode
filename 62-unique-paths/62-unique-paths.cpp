d871cbb09c546c0738a23b6df4ce3c74f26220d6
Naved Sohail <65990919+naved232doom@users.noreply.github.com>
2022-01-08 23:06:59 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (50.12%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(n,vector<int>(m));
-            dp[0][0]=1;
+        vector<int> dp(m);
+            dp[0]=1;
             for(int i=0;i<n;++i){
                     for(int j=0;j<m;++j){
-                            if(i-1>=0) dp[i][j]+=dp[i-1][j];
-                            if(j-1>=0) dp[i][j]+=dp[i][j-1];
+                            if(j-1>=0)
+                             dp[j]+=dp[j-1];                                      
                     }
             }
-            return dp[n-1][m-1];
+            return dp[m-1];
     }
 };
