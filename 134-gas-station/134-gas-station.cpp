b4648d418a87a30a15139a1a0c909514091c2982
Naved Sohail <65990919+naved232doom@users.noreply.github.com>
2022-01-21 19:17:17 +0530

    Time: 100 ms (23.68%), Space: 49.9 MB (45.28%) - LeetHub

--- 134-gas-station/134-gas-station.cpp
+++ 134-gas-station/134-gas-station.cpp
@@ -5,20 +5,18 @@ public:
         // 3 4 5 1 2 3 4 5 1 2
         int n = (int)(gas.size());
         int gas_sum = 0, cost_sum = 0;
-        for (int i = 0; i < n; ++i) {
-            gas_sum += gas[i];
-            cost_sum += cost[i];
-        }
-        if (gas_sum < cost_sum) return -1;
         int ans = 0;
         int sum_now = 0;
         for (int i = 0; i < n; ++i) {
-            sum_now += (gas[i] - cost[i]);
+            gas_sum += gas[i];
+            cost_sum += cost[i];
+        sum_now += (gas[i] - cost[i]);
             if (sum_now < 0) {
                 ans = i + 1;
                 sum_now = 0;
             }
         }
+        if (gas_sum < cost_sum) return -1;
         return ans;
 
     }
