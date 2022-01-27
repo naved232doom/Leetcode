50c175deb85807a30549b5d2a7ed735fe6fa133f
Naved Sohail <65990919+naved232doom@users.noreply.github.com>
2022-01-27 15:36:29 +0530

    Time: 336 ms (84.63%), Space: 115.2 MB (73.52%) - LeetHub

---
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    typedef long long ll;
+public:
+    long long mostPoints(vector<vector<int>>& questions) {
+        int n = (int)questions.size();
+        vector<ll> dp(n + 1);
+        for (int i = n - 1; i >= 0; --i) {
+            ll b_power = questions[i][1];
+            ll points_here = questions[i][0];
+            ll to_jump = min((ll)n, (ll)(i + b_power + 1));
+            dp[i] = max(dp[i], dp[to_jump] + points_here);
+            dp[i] = max(dp[i], dp[i + 1]);
+        }
+
+        return dp[0];
+    }
+};
