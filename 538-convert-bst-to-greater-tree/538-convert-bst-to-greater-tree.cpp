69bd9d714def29b57a85287e058e72bde26e0b33
Naved Sohail <65990919+naved232doom@users.noreply.github.com>
2022-04-16 17:37:08 +0530

    Time: 49 ms (61.70%), Space: 33.5 MB (62.62%) - LeetHub

--- 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
@@ -10,39 +10,19 @@
  * };
  */
 class Solution {
-    private: 
-    void dfs(TreeNode* root,vector<int> &order){
-        if(root==NULL) return;
+    private:
+    void dfs(TreeNode* root,int &sum){
+        if(root==NULL) return ;
         
-        dfs(root->left,order);
-        order.push_back(root->val);
-        dfs(root->right,order);
-    }
-    void dfs2(TreeNode* root,map<int,int> &pref){
-        if(root==NULL) return;
-        
-        root->val=pref[root->val];
-        dfs2(root->left,pref);
-        dfs2(root->right,pref);
+        dfs(root->right,sum);
+        sum+=root->val;
+        root->val=sum;
+        dfs(root->left,sum);
     }
 public:
     TreeNode* convertBST(TreeNode* root) {
-        vector<int> order;
-        dfs(root,order);
-        // for(int v:order) cout<<v<<" ";
-        // cout<<endl;
-        // 0 1 2 3 4 5 6 7 8
-        map<int,int> pref;
-        int n=(int)(order.size());
-        for(int i=n-1;i>=0;--i){
-            int here=order[i];
-            if(i+1<n){
-                order[i]+=order[i+1];
-            }
-            pref[here]=order[i];
-            //cout<<here<<" "<<pref[here]<<endl;
-        }
-        dfs2(root,pref);
+        int sum=0;
+        dfs(root,sum);
         return root;
     }
 };
