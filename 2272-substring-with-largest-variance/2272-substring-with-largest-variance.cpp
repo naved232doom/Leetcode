7a96032f6ce417cb405bd28575834a735b83b173
Naved Sohail <65990919+naved232doom@users.noreply.github.com>
2022-05-15 11:53:40 +0530

    Time: 456 ms (100.00%), Space: 7.5 MB (100.00%) - LeetHub

--- 2272-substring-with-largest-variance/2272-substring-with-largest-variance.cpp
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.cpp
@@ -1,7 +1,7 @@
 class Solution {
-public:
-    int largestVariance(string s) {
-        int n=(int)(s.length());
+    private:
+    int go(string s){
+         int n=(int)(s.length());
         set<char> st(s.begin(),s.end());
         int ans=0;
         for(char a:st){
@@ -29,32 +29,13 @@ public:
                 }
             }
         }
+        return ans;
+    }
+public:
+    int largestVariance(string s) {
+       int ans=go(s);
         reverse(s.begin(),s.end());
-        for(char a:st){
-            for(char b:st){
-                int var_here=0;
-                bool a_pres=0,b_pres=0;
-                for(int i=0;i<n;++i){
-                    if(s[i]!=a&&s[i]!=b) continue;
-                    if(s[i]==b){
-                        var_here++;
-                        b_pres=1;
-                    }
-                    else if(s[i]==a){
-                        var_here--;
-                        a_pres=1;
-                    }
-                    if(b_pres&&a_pres){
-                        ans=max(ans,var_here);
-                    }
-                    if(var_here<0){
-                        b_pres=a_pres=0;
-                        var_here=0;
-                    }
-                    
-                }
-            }
-        }
+        ans=max(ans,go(s));
         return ans;
     }
 };
