e67bf690a82ef5ff42df730f37a14d81f24cd974
Naved Sohail <65990919+naved232doom@users.noreply.github.com>
2022-05-24 17:40:37 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (42.55%) - LeetHub

---
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int ans=0;
+        int n=s.length();
+        vector<int> dp(n+1,0);
+        int open_cnt=0;
+        for(int i=0;i<n;++i){
+            if(s[i]=='(') open_cnt++;
+            else if(open_cnt>0){
+                dp[i]=max(dp[i],dp[i-1]+2);
+                int prev_idx=i-dp[i];
+                if(prev_idx>0){
+                    dp[i]+=dp[prev_idx];
+                }
+                open_cnt--;
+            }
+        }
+        
+        return *max_element(dp.begin(),dp.end());
+    }
+};
