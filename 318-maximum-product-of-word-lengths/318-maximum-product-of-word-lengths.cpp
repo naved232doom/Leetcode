19f234af7f78a1fcce72bbf2847740f9de8266be
Naved Sohail <65990919+naved232doom@users.noreply.github.com>
2022-05-29 10:01:17 +0530

    Time: 58 ms (73.14%), Space: 15.7 MB (64.52%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -3,6 +3,7 @@ public:
     int maxProduct(vector<string>& words) {
         int ans=0;
         int n=(int)(words.size());
+        if(n==1) return 0;
         vector<int> bits(n);
         for(int i=0;i<n;++i){
             for(char c:words[i]){
@@ -12,7 +13,7 @@ public:
         for(int i=0;i<n;++i){
             for(int j=i+1;j<n;++j){
                 if((bits[i]&bits[j])==0){
-                    ans=max(ans,(int)(words[i].length())*(int)(words[j].length()));
+                ans=max(ans,(int)(words[i].length())*(int)(words[j].length()));
                 }
             }
         }
