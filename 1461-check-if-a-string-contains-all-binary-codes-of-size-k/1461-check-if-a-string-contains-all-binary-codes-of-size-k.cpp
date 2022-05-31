46a6d2e575fc56f9dc6023cb5e439cdd8c8f82b3
Naved Sohail <65990919+naved232doom@users.noreply.github.com>
2022-05-31 16:39:03 +0530

    Time: 336 ms (70.44%), Space: 40 MB (77.04%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -3,16 +3,21 @@ public:
     bool hasAllCodes(string s, int k) {
         // all numbers from 0 to exp(2,k+1)-1
         int n=(int)(s.length());
+        if(n<k) return 0;
         const int MAX=(1<<(k+1));
         unordered_set<int> f;
-        
-        for(int i=0,j=i+k-1;i<n&&j<n;++i,++j){
-            int val_here=0;
-            for(int l=0,p=j;l<k;++l,--p){
+        int val_here=0;
+        for(int l=0,p=l+k-1;l<k;++l,--p){
                 if(s[p]=='1'){
                     val_here|=(1<<l);
                 }
-            }
+        }
+        f.insert(val_here);
+        for(int i=1,j=i+k-1;i<n&&j<n;++i,++j){
+            int prev_idx=i-1;
+            val_here-=(s[prev_idx]=='1' ? (1<<(k-1)):0);
+            val_here*=2;
+            val_here+=(s[j]=='1' ? 1:0);
             assert(val_here<(MAX));
             if(f.find(val_here)!=f.end()) continue;
             f.insert(val_here);
