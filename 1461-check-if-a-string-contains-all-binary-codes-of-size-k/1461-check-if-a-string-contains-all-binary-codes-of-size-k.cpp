6d3ac36f4871bfc3814883d17298ada5aadf320f
Naved Sohail <65990919+naved232doom@users.noreply.github.com>
2022-05-31 16:39:28 +0530

    Time: 208 ms (83.02%), Space: 39.9 MB (80.19%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -2,28 +2,28 @@ class Solution {
 public:
     bool hasAllCodes(string s, int k) {
         // all numbers from 0 to exp(2,k+1)-1
-        int n=(int)(s.length());
-        if(n<k) return 0;
-        const int MAX=(1<<(k+1));
+        int n = (int)(s.length());
+        if (n < k) return 0;
+        const int MAX = (1 << (k + 1));
         unordered_set<int> f;
-        int val_here=0;
-        for(int l=0,p=l+k-1;l<k;++l,--p){
-                if(s[p]=='1'){
-                    val_here|=(1<<l);
-                }
+        int val_here = 0;
+        for (int l = 0, p = l + k - 1; l < k; ++l, --p) {
+            if (s[p] == '1') {
+                val_here |= (1 << l);
+            }
         }
         f.insert(val_here);
-        for(int i=1,j=i+k-1;i<n&&j<n;++i,++j){
-            int prev_idx=i-1;
-            val_here-=(s[prev_idx]=='1' ? (1<<(k-1)):0);
-            val_here*=2;
-            val_here+=(s[j]=='1' ? 1:0);
-            assert(val_here<(MAX));
-            if(f.find(val_here)!=f.end()) continue;
+        for (int i = 1, j = i + k - 1; i < n && j < n; ++i, ++j) {
+            int prev_idx = i - 1;
+            val_here -= (s[prev_idx] == '1' ? (1 << (k - 1)) : 0);
+            val_here *= 2;
+            val_here += (s[j] == '1' ? 1 : 0);
+            assert(val_here < (MAX));
+            if (f.find(val_here) != f.end()) continue;
             f.insert(val_here);
         }
-        int cnt= (int)(f.size());
+        int cnt = (int)(f.size());
         //cout<<cnt<<endl;
-        return cnt==(1<<k) ;
+        return cnt == (1 << k) ;
     }
 };
