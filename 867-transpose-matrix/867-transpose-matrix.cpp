c4aaee61f0baeeb1f7168f0f80a6c41c18183bca
Naved Sohail <65990919+naved232doom@users.noreply.github.com>
2022-06-02 18:39:33 +0530

    Time: 19 ms (34.70%), Space: 10.6 MB (43.03%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.cpp
+++ 867-transpose-matrix/867-transpose-matrix.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> transpose(vector<vector<int>>& mt) {
-        int n=(int)(mt.size());
-        int m=(int)(mt[0].size());
-    if(n==m){
-        for(int i=0;i<n;++i){
-            for(int j=i;j<n;++j){
-                swap(mt[i][j],mt[j][i]);
+        int n = (int)(mt.size());
+        int m = (int)(mt[0].size());
+        if (n == m) {
+            for (int i = 0; i < n; ++i) {
+                for (int j = i; j < n; ++j) {
+                    swap(mt[i][j], mt[j][i]);
+                }
             }
+            return mt;
         }
-        return mt;
-    }
-        
-       vector<vector<int>> ans(m,vector<int>(n));
-        for(int i=0;i<n;++i){
-            for(int j=0;j<m;++j){
-                ans[j][i]=mt[i][j];
+
+        vector<vector<int>> ans(m, vector<int>(n));
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                ans[j][i] = mt[i][j];
             }
         }
         return ans;
