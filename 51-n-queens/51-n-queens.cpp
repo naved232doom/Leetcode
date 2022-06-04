a087f0a4e1d215334869e2dddf8a5d629a79bc79
Naved Sohail <65990919+naved232doom@users.noreply.github.com>
2022-06-04 23:47:37 +0530

    Time: 103 ms (7.55%), Space: 26.7 MB (18.69%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,56 +1,47 @@
 class Solution {
-public:void dfs(vector<vector<string>> &ans,vector<int> r_c,vector<int> c_c,vector<string> g,int &n,int &m,int r_here=0){
-        if(r_here==n){
+public:
+    void dfs(vector<vector<string>> &ans, vector<int> r_c, vector<int> c_c, vector<string> g, int &n, set<int> mp, set<int> mp2, int r_here = 0) {
+
+        if (r_here == n) {
             ans.push_back(g);
             return;
         }
+        for (int j = 0, i = r_here; j < n; ++j) {
+            if (c_c[j] != -1) continue;
+            r_c[i] = j;
+            c_c[j] = i;
+            g[i][j] = 'Q';
 
-        for(int j=0,i=r_here;j<m;++j){
-            if(c_c[j]!=-1) continue;
-                r_c[i]=j;
-                c_c[j]=i;
-                g[i][j]='Q';
-             if(works(g,r_here,j,n,m)){
-                    
-                 dfs(ans,r_c,c_c,g,n,m,r_here+1);
-             }
-                r_c[i]=-1;
-                c_c[j]=-1;
-                g[i][j]='.';
-            
+            if (!mp.count(i - j) && !mp2.count(i + j)) {
+                mp.insert(i - j);
+                mp2.insert(i + j);
+                dfs(ans, r_c, c_c, g, n, mp, mp2, r_here + 1);
+                mp.erase(i - j);
+                mp2.erase(i + j);
+            }
+            r_c[i] = -1;
+            c_c[j] = -1;
+            g[i][j] = '.';
+
         }
-        
+
     }
-    bool works(vector<string> g,int x,int y,int n,int m){
-        bool ok=1;
-        vector<int> dx={-1,1,1,-1};
-        vector<int> dy={1,-1,1,-1};
-    for(int i=0;i<4;++i){
-        int t_x=x+dx[i],t_y=y+dy[i];
-        while(valid_xy(t_x,t_y,n,m)){
-           if(g[t_x][t_y]=='Q') return 0;
-            t_x+=dx[i];
-            t_y+=dy[i];
-        }
-    }
-        
-        return 1;
-    }
-    bool valid_xy(int x,int y,int n,int m){
-        return x>=0&&y>=0&&x<n&&y<m;
+    bool valid_xy(int x, int y, int n, int m) {
+        return x >= 0 && y >= 0 && x < n && y < m;
     }
     vector<vector<string>> solveNQueens(int n) {
         vector<string> g;
-        for(int i=0;i<n;++i){
-            string h="";
-            for(int j=0;j<n;++j){
-                h+='.';
+        for (int i = 0; i < n; ++i) {
+            string h = "";
+            for (int j = 0; j < n; ++j) {
+                h += '.';
             }
             g.push_back(h);
         }
+        set<int> mp, mp2;
         vector<vector<string>> ans;
-        vector<int> r_c(n,-1),c_c(n,-1);
-        dfs(ans,r_c,c_c,g,n,n);
+        vector<int> r_c(n, -1), c_c(n, -1);
+        dfs(ans, r_c, c_c, g, n, mp, mp2);
         return ans;
     }
 };
