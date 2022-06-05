8072acef7204a77af7830cb7a87b3485ab8f3b09
Naved Sohail <65990919+naved232doom@users.noreply.github.com>
2022-06-05 12:29:58 +0530

    Time: 42 ms (17.39%), Space: 20.3 MB (13.62%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+private: void solve(vector<vector<int>> &g, int &n, int &ans, vector<int> c_p, map<int, int> m1, map<int, int> m2, int r_here = 0) {
+        if (r_here == n) {
+            ans++;
+            return;
+        }
+        for (int j = 0, i = r_here; j < n; ++j) {
+            if (c_p[j] != -1) continue;
+            int sum = i + j, dif = i - j;
+            if (m1.count(sum) || m2.count(dif)) continue;
+            c_p[j] = i;
+            m1.insert({sum, 1});
+            m2.insert({dif, 1});
+            g[i][j] = 1;
+            solve(g, n, ans, c_p, m1, m2, r_here + 1);
+            c_p[j] = -1;
+            g[i][j] = 0;
+            m1.erase(sum);
+            m2.erase(dif);
+        }
+
+    }
+public:
+    int totalNQueens(int n) {
+        vector<vector<int>> g(n, vector<int>(n, 0));
+        int ans = 0;
+        vector<int> c_p(n, -1);
+        map<int, int> m1, m2;
+        solve(g, n, ans, c_p, m1, m2);
+        return ans;
+    }
+};
