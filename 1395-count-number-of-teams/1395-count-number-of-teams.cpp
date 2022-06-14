9bc48ef4199064f7f71b60278ef8dbbc2b99ce55
Naved Sohail <65990919+naved232doom@users.noreply.github.com>
2022-06-14 22:08:40 +0530

    Time: 110 ms (82.40%), Space: 10 MB (16.62%) - LeetHub

---
+++ 1395-count-number-of-teams/1395-count-number-of-teams.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    struct bit {
+        int n;
+        vector<long long> a;
+
+        void init(int s) {
+            a = vector<long long>(s + 1, 0);
+            n = s;
+        }
+
+        void add(int loc, long long x) {
+            if (loc <= 0) return;
+            while (loc <= n) {
+                a[loc] += x;
+                loc += loc & (-loc);
+            }
+        }
+
+        long long query(int loc) {
+            long long sum = 0;
+            while (loc > 0) {
+                sum += a[loc];
+                loc -= loc & (-loc);
+            }
+            return sum;
+        }
+    };
+
+    void coordinate_compression(vector<int> &a) {
+        auto d = a;
+        int n = (int)(d.size());
+        sort(d.begin(), d.end());
+        d.resize(unique(d.begin(), d.end()) - d.begin());
+
+        for (int i = 0; i < n; ++i) {
+            a[i] = lower_bound(d.begin(), d.end(), a[i]) - d.begin() + 1;
+        }
+    }
+    int solve(vector<int> &arr) {
+        bit bit;
+        int n = (int)(arr.size());
+        bit.init(n + 1);
+        vector<int> dp(n);
+        for (int i = n - 1; i >= 0; --i) {
+            dp[i] = bit.query(n + 1) - bit.query(arr[i]);
+            bit.add(arr[i], 1);
+        }
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                if (arr[i] > arr[j]) continue;
+                ans += dp[j];
+            }
+        }
+        return ans;
+    }
+    int numTeams(vector<int>& arr) {
+        int ans = 0;
+        coordinate_compression(arr);
+        ans += solve(arr);
+        reverse(arr.begin(), arr.end());
+        ans += solve(arr);
+        // 2 5 3 4 1
+        // 1 4 3 5 2
+        return ans;
+    }
+};
