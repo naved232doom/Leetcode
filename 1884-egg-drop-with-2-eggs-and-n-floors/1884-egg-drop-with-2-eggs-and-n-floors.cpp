f7f3c220210462ac0d274a013060a584d58f78cf
Naved Sohail <65990919+naved232doom@users.noreply.github.com>
2022-06-14 21:47:33 +0530

    Time: 4 ms (72.79%), Space: 6 MB (47.14%) - LeetHub

--- 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
+++ 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
@@ -21,11 +21,12 @@ public:
     int twoEggDrop(int n) {
         int K=2;
        int ans=0;
-        vector<vector<int>> dp(n+1,vector<int>(K+1));
-        while(dp[ans][K]< n){
+        vector<vector<int>> dp(2,vector<int>(K+1));
+        int res=0;
+        while(dp[ans%2][K]< n){
             ans++;
             for(int i=1;i<=K;++i){
-                dp[ans][i]=dp[ans-1][i-1]+dp[ans-1][i]+1;
+                dp[ans%2][i]=dp[(ans+1)%2][i-1]+dp[(ans+1)%2][i]+1;
             }
         }
         return ans;
