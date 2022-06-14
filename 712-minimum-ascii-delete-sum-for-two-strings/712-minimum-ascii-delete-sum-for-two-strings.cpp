1ceda8fbc37577c402ca97e088f4daea328ea8f2
Naved Sohail <65990919+naved232doom@users.noreply.github.com>
2022-06-14 14:14:17 +0530

    Time: 75 ms (26.80%), Space: 15.1 MB (57.67%) - LeetHub

--- 712-minimum-ascii-delete-sum-for-two-strings/712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 712-minimum-ascii-delete-sum-for-two-strings/712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     int minimumDeleteSum(string s1, string s2) {
-        int n=s1.length(),m=s2.length();
-        const int INF=(int)1e9;
-        vector<vector<int>> dp(n+1,vector<int>(m+1,INF));
-        for(int i=0,S=0;i<=n;++i){
-            dp[i][0]=S;
-            S+= ( i < n ? s1[i]:0);
-        } 
-        for(int i=0,S=0;i<=m;++i){
-            dp[0][i]=S;
-            S+= ( i < m ? s2[i]:0);
-        }
+        int n = s1.length(), m = s2.length();
+        const int INF = (int)1e9;
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1));
+        int sum=0;
         for(int i=0;i<n;++i){
-            for(int j=0;j<m;++j){
-                dp[i+1][j+1]=min(dp[i+1][j+1],dp[i+1][j]+(int)s2[j]);
-                dp[i+1][j+1]=min(dp[i+1][j+1],dp[i][j+1]+(int)s1[i]);
-                if(s1[i]==s2[j]){
-                    dp[i+1][j+1]=min(dp[i+1][j+1],dp[i][j]);
+            sum+=s1[i];
+        }
+        for(int i=0;i<m;++i){
+            sum+=s2[i];
+        }
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+            dp[i + 1][j + 1] = max(dp[i + 1][j + 1], dp[i + 1][j]) ;
+            dp[i + 1][j + 1] = max(dp[i + 1][j + 1], dp[i][j + 1]);
+                if (s1[i] == s2[j]) {
+            dp[i + 1][j + 1] = max(dp[i + 1][j + 1], dp[i][j] + (int)(s1[i]));
                 }
             }
         }
-        
-        return dp[n][m];
+
+        return sum- 2*dp[n][m] ;
     }
 };
