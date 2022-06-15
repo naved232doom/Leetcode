ae67ecf789ec157318f3192cdd54d2e97bbe80e5
Naved Sohail <65990919+naved232doom@users.noreply.github.com>
2022-06-15 10:28:21 +0530

    Time: 74 ms (89.41%), Space: 18.6 MB (45.10%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,34 +1,42 @@
 class Solution {
-    int is_subs(string &a, string &b){
-        int n=a.length(),m=b.length();
-        int i=0,j=0;
-        for(;i<n&&j<m;){
-            if(a[i]==b[j]){
-                i++;j++;
+    int is_subs(string &a, string &b) {
+        int n = a.length(), m = b.length();
+        int i = 0, j = 0;
+        for (; i < n && j < m;) {
+            if (a[i] == b[j]) {
+                i++; j++;
             }
-            else{
+            else {
                 j++;
             }
         }
-        return i==n&&m==n+1;
+        return i == n && m == n + 1;
     }
 public:
     int longestStrChain(vector<string>& words) {
-        sort(words.begin(),words.end(),[](string a,string b){
-            return (int)a.length()< (int)b.length();
-        });
-        int n=(int)(words.size());
-        vector<int> dp(n,1);
-        for(int i=0;i<n;++i){
-            for(int j=i+1;j<n;++j){
-                if(words[j].length()!=1+words[i].length()) continue;
-                if(words[j].length()>1+words[i].length()) break;
-                
-                if(is_subs(words[i],words[j])){
-                    dp[j]=max(dp[j],dp[i]+1);
+        // sort(words.begin(),words.end(),[](string a,string b){
+        //     return (int)a.length()< (int)b.length();
+        // });
+        int n = (int)(words.size());
+        vector<int> dp(n, 1);
+        vector<vector<pair<string, int>>> adj(20);
+        int it = 0;
+        for (auto s : words) {
+            adj[s.length()].push_back({s, it++});
+        }
+        int ans = 1;
+        for (int i = 0; i < 18; ++i) {
+            for (auto q : adj[i]) {
+                for (auto p : adj[i + 1]) {
+                    auto idx = p.second;
+                    auto s_here = p.first;
+                    if (is_subs(q.first, s_here)) {
+                        dp[idx] = max(dp[idx], dp[q.second] + 1);
+                        ans = max(ans, dp[idx]);
+                    }
                 }
             }
         }
-        return *max_element(dp.begin(),dp.end());
+        return ans;
     }
 };
