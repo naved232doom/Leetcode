29403a857c9e8641a59875e9e06e9b118841fc78
Naved Sohail <65990919+naved232doom@users.noreply.github.com>
2022-06-15 10:20:21 +0530

    Time: 547 ms (8.85%), Space: 22.9 MB (29.54%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    int is_subs(string &a, string &b){
+        int n=a.length(),m=b.length();
+        int i=0,j=0;
+        for(;i<n&&j<m;){
+            if(a[i]==b[j]){
+                i++;j++;
+            }
+            else{
+                j++;
+            }
+        }
+        return i==n&&m==n+1;
+    }
+public:
+    int longestStrChain(vector<string>& words) {
+        sort(words.begin(),words.end(),[](string a,string b){
+            return (int)a.length()< (int)b.length();
+        });
+        int n=(int)(words.size());
+        vector<int> dp(n,1);
+        for(int i=0;i<n;++i){
+            for(int j=i+1;j<n;++j){
+                if(is_subs(words[i],words[j])){
+                    dp[j]=max(dp[j],dp[i]+1);
+                }
+            }
+        }
+        return *max_element(dp.begin(),dp.end());
+    }
+};
