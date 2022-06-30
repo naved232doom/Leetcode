092dab46b41bbe052a952ab005c4fd6a98b33d86
Naved Sohail <65990919+naved232doom@users.noreply.github.com>
2022-06-30 17:23:18 +0530

    Time: 265 ms (27.79%), Space: 90 MB (5.81%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
@@ -1,15 +1,41 @@
 class Solution {
 public:
-  vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
-        sort(people.begin(), people.end(), cmp);
-        vector<vector<int>> res;
-        for(int i = 0; i < people.size(); i++){
-            res.insert(res.begin()+people[i][1], people[i]);
-        }
-        return res;
-    }
-    static bool cmp(vector<int>& p1, vector<int>& p2){
+     static bool cmp(vector<int>& p1, vector<int>& p2){
         if(p1[0]!=p2[0]) return p1[0]>p2[0];
         else return p1[1]<p2[1];
+    }
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& X) {
+        int n=(int)(X.size());
+        vector<array<int,3>> P;
+        for(int i=0;i<n;++i){
+            P.push_back({X[i][0],X[i][1],i});
+        }
+        sort(P.begin(),P.end(),[](array<int,3> &a,array<int,3> &b){
+            if(a[0]!=b[0]) return a[0]>b[0];
+            return a[1]<b[1];
+        });
+        // 7,0 7,1
+        vector<vector<int>> ans;
+        vector<int> ans_idx;
+        for(int i=0;i<n;++i){
+            int idx= P[i][1];
+            vector<int> pref;
+            assert(idx<=(int)(ans_idx.size()));
+            for(int j=0;j<idx;++j){
+                pref.push_back(ans_idx[j]);
+            }
+            pref.push_back(P[i][2]);
+            for(int j=idx;j<(int)ans_idx.size();++j){
+                pref.push_back(ans_idx[j]);
+            }
+            ans_idx.clear();
+            for(auto x:pref) ans_idx.push_back(x);
+        }
+        for(int i=0;i<n;++i){
+            ans.push_back(X[ans_idx[i]]);
+        }
+        return ans;
+        
+      
     }
 };
