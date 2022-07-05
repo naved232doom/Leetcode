0231ba6ed73e0aed362315d216e0a4e2a71b26b8
Naved Sohail <65990919+naved232doom@users.noreply.github.com>
2022-07-05 21:59:56 +0530

    Time: 480 ms (41.00%), Space: 70 MB (5.02%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -4,17 +4,36 @@ public:
         int n=(int)(arr.size());
         if(n<=1) return n;
         sort(arr.begin(),arr.end());
-        int ans=1;
-        int cnt=1;
-        for(int i=1;i<n;++i){
-            if(arr[i]==arr[i-1]) continue;
-            if(arr[i]!=arr[i-1]+1){
-                ans=max(ans,cnt);
-                cnt=1;
+        vector<vector<int>> adj(n);
+        map<int,int> mp;
+        for(int i=0;i<n;++i) mp[arr[i]]=i;
+        vector<int> deg(n);
+        for(int i=0;i<n;++i){
+            if(mp.find(arr[i]+1)!=mp.end()){
+                adj[i].push_back(mp[arr[i]+1]);
+                deg[mp[arr[i]+1]]++;
             }
-            else cnt++;
         }
-        ans=max(ans,cnt);
+        queue<int> q;
+        vector<int> dp(n);
+        for(int i=0;i<n;++i){
+            if(deg[i]==0) {
+                q.push(i);
+                dp[i]=1;
+            }
+        }
+        while(!q.empty()){
+            auto u=q.front();q.pop();
+            
+            for(int v:adj[u]){
+                deg[v]--;
+                if(deg[v]<=1){
+                    dp[v]=max(dp[v],dp[u]+1);
+                    q.push(v);
+                }
+            }
+        }
+        int ans=*max_element(dp.begin(),dp.end());
         return ans;
     }
 };
