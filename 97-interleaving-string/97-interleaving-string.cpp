267f079eb403a00109b6cc639cfce4edd8db3ebe
Naved Sohail <65990919+naved232doom@users.noreply.github.com>
2022-07-07 21:11:03 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (96.49%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -4,34 +4,17 @@ public:
         int it1 = 0, it2 = 0;
         int n1 = s1.length(), n2 = s2.length(), n3 = s3.length();
         if(n1+n2 != n3) return false;
-        queue<array<int, 2>> q;
-        vector<vector<int>> vis(n1+1,vector<int>(n2+1));
-        q.push({0, 0});
-        vis[0][0]=1;
-        while (!q.empty()) {
-            auto cur = q.front();
-            q.pop();
-            int idx3 = cur[0] + cur[1];
-            // cout<<"cur[0]: "<<cur[0]<<endl;
-            // cout<<"cur[1]: "<<cur[1]<<endl;
-            if (cur[0] == n1 && cur[1] == n2) return true;
-            if (cur[0] < n1 && s1[cur[0]] == s3[idx3]) {
-                if(!vis[cur[0]+1][cur[1]]) {
-                q.push({cur[0] + 1, cur[1]});
-                vis[cur[0]+1][cur[1]]=1;
-                    
-                }
+        int dp[n1+2][n2+2];
+        memset(dp,0,sizeof dp);
+        dp[0][0]=1;
+ 
+        for(int i=0;i<=n1;++i){
+            for(int j=0;j<=n2;++j){
+                if(dp[i][j] && i<n1 && s1[i]==s3[i+j]) dp[i+1][j]=1;
+                if(dp[i][j] && j<n2 && s2[j]==s3[i+j]) dp[i][j+1]=1;
+               
             }
-            if (cur[1] < n2 && s2[cur[1]] == s3[idx3]) {
-                if(!vis[cur[0]][cur[1]+1]) {
-                q.push({cur[0], cur[1] + 1});
-                vis[cur[0]][cur[1]+1]=1;
-                    
-                }
-            }
-
         }
-
-        return false;
+        return dp[n1][n2];
     }
 };
