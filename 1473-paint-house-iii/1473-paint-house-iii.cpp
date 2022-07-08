35dcf480594f6c1246c07ea1f71c93b0ad0419b9
Naved Sohail <65990919+naved232doom@users.noreply.github.com>
2022-07-08 22:08:12 +0530

    Time: 182 ms (30.32%), Space: 10.7 MB (92.29%) - LeetHub

---
+++ 1473-paint-house-iii/1473-paint-house-iii.cpp
@@ -0,0 +1,70 @@
+class Solution {
+public:
+    int minCost(vector<int>& H, vector<vector<int>>& cost, int n, int m, int T) {
+        // dp[i][j][k]
+        for (auto &h : H) {
+            h--;
+        }
+        
+        int dp[n + 1][T + 1][m + 1];
+        const int INF = (int)(1e9);
+        for (int i = 0; i <= n; ++i) {
+            for (int j = 0; j <= T; ++j) {
+                for (int k = 0; k <= m; ++k) {
+                    dp[i][j][k] = INF;
+                }
+            }
+        }
+        //
+        
+        for (int i = 0; i <= m; ++i) dp[0][0][i] = 0;
+        if(H.front()==-1){
+            for(int c=0;c<m;++c){
+                dp[1][1][c]=cost[0][c];
+            }
+        }
+        else{
+            dp[1][1][H[0]]=cost[0][H[0]];
+        }
+     
+        for (int i = 0; i < n; ++i) {
+
+            for (int j = 1; j <= T; ++j) {
+                for (int k = 0; k < m; ++k) {
+                    if (H[i] >= 0) {
+                        if (k == H[i]) {
+                    int j_here=max(j,1);
+    dp[i + 1][j][H[i]] = min(dp[i + 1][j][H[i]], dp[i][j][k] + cost[i][H[i]]);
+                        }
+                        else if(j<T) {
+
+        dp[i + 1][j + 1][H[i]] = min(dp[i + 1][j + 1][H[i]], dp[i][j][k] + cost[i][H[i]]);
+                        }
+                        continue;
+                    }
+                    for (int l = 0; l < m; ++l) {
+                        if (k == l) {
+                            int j_here=max(j,1);
+            dp[i + 1][j][l] = min(dp[i + 1][j][l], dp[i][j][k] + cost[i][l]);
+                        }
+                        else if(j<T){
+            dp[i + 1][j + 1][l] = min(dp[i + 1][j + 1][l], dp[i][j][k] + cost[i][l]);
+
+                        }
+                    }
+                }
+            }
+        }
+        int prev=0;
+        for(int i=0;i<n;++i){
+            if(H[i]>=0){
+                prev+=cost[i][H[i]];
+            }
+        }
+        int ans = INF;
+        for (int i = 0; i < m; ++i) {
+            ans = min(ans, dp[n][T][i]);
+        }
+        return (ans== INF ? -1:ans-prev);
+    }
+};
