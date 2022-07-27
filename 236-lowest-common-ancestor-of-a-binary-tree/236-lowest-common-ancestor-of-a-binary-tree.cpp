6922666a802f990098c195b483f9b60179ada7fe
Naved Sohail <65990919+naved232doom@users.noreply.github.com>
2022-07-27 18:28:00 +0530

    Time: 17 ms (87.17%), Space: 14.2 MB (85.91%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,28 +9,18 @@
  */
 class Solution {
 public:
-    vector<int> lca(TreeNode* root, TreeNode* p, TreeNode* q,TreeNode* &ans,vector<int> present){
-        
-        if(root==nullptr) return {0,0};
-        // p.val==root->val => present[0]=1;
-        auto left=lca(root->left,p,q,ans,present);
-        auto right=lca(root->right,p,q,ans,present);
-        for(int i=0;i<2;++i){
-            present[i]|=left[i];
-            present[i]|=right[i];
-        }
-        // if(root->val==3){
-        //     cout<<present[0]<<" "<<present[1]<<endl;
-        // }
-         if(p->val==root->val) present[0]=1;
-        if(q->val==root->val) present[1]=1;
-        if(present[0] && present[1] && !ans) ans=root;
-        return present;
+    TreeNode* lca(TreeNode* root, TreeNode* p, TreeNode* q){
+        if(root==nullptr) return root;
+        if(root==p) return root;
+        if(root==q) return root;
+        auto left=lca(root->left,p,q);
+        auto right=lca(root->right,p,q);
+        if(left && right) return root;
+        if(left) return left;
+        return right;
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<int> present(2);
-        TreeNode* ans=nullptr;
-        lca(root,p,q,ans,present);
-        return ans;
+        return lca(root,p,q);
+        
     }
 };
