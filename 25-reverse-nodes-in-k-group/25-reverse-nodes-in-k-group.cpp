fbfcda105ea8dbfabfba185fb798ca2d465f3de9
Naved Sohail <65990919+naved232doom@users.noreply.github.com>
2022-07-29 11:30:04 +0530

    Time: 27 ms (40.33%), Space: 11.5 MB (71.56%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
@@ -10,36 +10,36 @@
  */
 class Solution {
 public:
-    ListNode* reverse(ListNode* begin,ListNode* end){
-        ListNode* cur=begin->next;
-        ListNode* prev=begin;
-        ListNode* first= begin->next;
-        while(cur!=end){
-            ListNode* nxt=cur->next;
-            cur->next=prev;
-            prev=cur;
-            cur=nxt;
-        }
-        begin->next=prev;
-        first->next=cur;
-        return first;
-    }
+   
     ListNode* reverseKGroup(ListNode* head, int k) {
         // -1->1->2->3->4->5
         // -1->2 <-1<-2    3->4->5
-                   // prev  cur     
-        ListNode* dummy= new ListNode(-1);
-        dummy->next=head;
-        ListNode* ans=dummy;
-        ListNode* begin=dummy;
-        int it=0;
-        while(head!=nullptr){
+        // prev  cur
+        ListNode* dummy = new ListNode(-1);
+        dummy->next = head;
+        ListNode* ans = dummy;
+        ListNode* begin = dummy;
+        int it = 0;
+        while (head != nullptr) {
             it++;
-            if(it%k==0){
-                begin=reverse(begin,head->next);
-                head=begin->next;
+            if (it % k == 0) {
+                //begin = reverse(begin, head->next);
+                ListNode* end=head->next;
+                ListNode* cur = begin->next;
+                ListNode* prev = begin;
+                ListNode* first = begin->next;
+                while (cur != end) {
+                    ListNode* nxt = cur->next;
+                    cur->next = prev;
+                    prev = cur;
+                    cur = nxt;
+                }
+                begin->next = prev;
+                first->next = cur;
+                begin=first;
+                head = begin->next;
             }
-            else head=head->next;
+            else head = head->next;
         }
         return ans->next;
     }
