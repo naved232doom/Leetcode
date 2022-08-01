8ef245b4339f06d60c0a80bc78e0ce576a396f6f
Naved Sohail <65990919+naved232doom@users.noreply.github.com>
2022-08-02 02:59:00 +0530

    Time: 763 ms (28.28%), Space: 166.5 MB (62.50%) - LeetHub

---
+++ 146-lru-cache/146-lru-cache.cpp
@@ -0,0 +1,86 @@
+class LRUCache {
+public:
+    class Node{
+        public:
+        Node* next;
+        Node* prev;
+        int key;
+        int value;
+        Node(int key,int value){
+            this->key=key;
+            this->value=value;
+            next=NULL;
+            prev=NULL;
+        }
+        
+    };
+    map<int,Node*> address;
+    map<int,int> mp;
+    Node* head;
+    Node* tail;
+    int max_size=0;
+    int cur_size=0;
+    LRUCache(int capacity) {
+        max_size=capacity;
+        head=new Node(-1,-1);
+        tail=new Node(-1,-1);
+        head->next=tail;
+        tail->prev=head;
+    }
+    void moveNewNodeToHead(int key){
+        Node* cur=address[key];
+        Node* P=cur->prev;
+        Node* N=cur->next;
+        P->next=N;
+        N->prev=P;
+        N=head->next;
+        // add cur node to head
+        head->next=cur;
+        cur->prev=head;
+        cur->next=N;
+        N->prev=cur;
+    }
+    int get(int key) {
+        if(mp.find(key)==mp.end()) return -1;
+        // fetch the value
+        // move it towards the tail
+        // isolate cur node
+        moveNewNodeToHead(key);
+        return mp[key];
+    }
+    
+    void put(int key, int value) {
+        if(mp.find(key)!=mp.end()){
+            mp[key]=value;
+            moveNewNodeToHead(key);
+            return;
+        }
+        Node* previous=tail->prev;
+        //assert(previous->prev!=nullptr);
+        Node* prev2=previous->prev;    
+        if(cur_size==max_size){
+            cur_size--;              
+            prev2->next=tail;
+            tail->prev=prev2;
+            mp.erase(previous->key);
+            delete(previous);
+        }
+        
+        Node* cur=new Node(key,value);
+        Node* head_next=head->next;
+        head->next=cur;
+        cur->next=head_next;
+        cur->prev=head;
+        head_next->prev=cur;
+        mp[key]=value;
+        address[key]=cur;
+        cur_size++;
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
