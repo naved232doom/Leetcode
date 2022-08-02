c70d91590dcbac90ba24c5675f3dcc032e2d57b0
Naved Sohail <65990919+naved232doom@users.noreply.github.com>
2022-08-02 17:53:28 +0530

    Time: 41 ms (47.42%), Space: 25.8 MB (97.73%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,30 +11,26 @@
  */
 class Solution {
 public:
-    map<int,int> inorder_idx;
-    TreeNode* dfs(vector<int> &preorder, vector<int> &inorder, int l_in, int r_in, int l_pre) {
-
-        if (l_in > r_in or l_pre >= preorder.size()) return nullptr;
-
-        int idx_root = -1;
-        idx_root=inorder_idx[preorder[l_pre]];
-        assert(idx_root != -1);
-        TreeNode* root = new TreeNode(preorder[l_pre]);
-
-        int cnt_left_subtree = idx_root - l_in + 1;
-
-        root->left = dfs(preorder, inorder, l_in, idx_root - 1, l_pre + 1);
-        root->right = dfs(preorder, inorder, idx_root + 1, r_in, l_pre + cnt_left_subtree);
-
-        return root;
-
+    TreeNode* dfs(vector<int>& preorder, vector<int>& inorder,int l_in,int r_in,int l_pre){
+        if(l_in > r_in || l_pre>=preorder.size()) return nullptr;
+        
+        int node=preorder[l_pre];
+        int node_idx;
+        for(int i=l_in;i<=r_in;++i){
+            if(node==inorder[i]) node_idx=i;
+        }
+        int left_count = node_idx-l_in+1;
+        int right_count = (int)(inorder.size())- node_idx+1;
+        
+        TreeNode* cur=new TreeNode(preorder[l_pre]);
+        
+        cur->left=dfs(preorder,inorder,l_in,node_idx-1,l_pre+1);
+        cur->right=dfs(preorder,inorder,node_idx+1,r_in,l_pre+left_count);
+        return cur;
+        
     }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        // preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
-        // bottom-line=> we get lengths of left and right subtree from inorder
-        for(int i=0;i<inorder.size();++i){
-            inorder_idx[inorder[i]]=i;
-        }
-        return dfs(preorder, inorder, 0, inorder.size(), 0);
+        int n=(int)(preorder.size());
+        return dfs(preorder,inorder,0,n-1,0);
     }
 };
