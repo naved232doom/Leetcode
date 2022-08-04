cfddb7694ca401c7b4cf37d46ce9d9ea6cf04f33
Naved Sohail <65990919+naved232doom@users.noreply.github.com>
2022-08-04 14:33:01 +0530

    Time: 490 ms (29.83%), Space: 283.9 MB (13.36%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,31 +1,37 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        if(s.size()<=0)
-            return "";
-        if(s.size()==1)
-            return s;
-        int n = (int)(s.size());
-        vector<vector<int>> dp(n, vector<int>(n));
-        for (int i = 0; i < n; ++i) dp[i][i] = 1;
-        int ans = 1, L = 0, R = 0;
-        for (int len = 2; len <= n; ++len) {
-            for (int i = 0; i < n; ++i) {
-                int r = i + len - 1;
-                int l = i;
-                if (r >= n) continue;
-                if (s[i] != s[r]) continue;
-                if (r - 1 < l + 1 || dp[l + 1][r - 1]) {
-                    dp[l][r] |= 1;
-                    if (len > ans) {
-                        ans = len;
-                        L = l;
-                        R = r;
-                    }
+        int n=s.length();
+        vector<vector<int>> dp(n+1,vector<int>(n+1));
+        for(int i=0;i<n;++i) dp[i][i]=1;
+        
+        // cbabcad
+        for(int j=0;j<n;++j){
+            for(int i=0;i<=j;++i){
+                if(s[i]!=s[j]) continue;
+                int I=i+1,J=j-1;
+                if(j>i && j-i+1==2) 
+                {
+                    dp[i][j]=2;
+                    continue;
                 }
-
+                if(I>J) continue;
+                if(dp[I][J]) dp[i][j]=1;
             }
         }
-        return s.substr(L, R - L + 1);
+        int len=1;
+        int beg=0;
+        for(int i=0;i<n;++i){
+            for(int j=i;j<n;++j){
+                if(!dp[i][j]) continue;
+                int len_here=j-i+1;
+                if(len_here>len){
+                    len=len_here;
+                    beg=i;
+                }
+            }
+        }
+        return s.substr(beg,len);
+       
     }
 };
