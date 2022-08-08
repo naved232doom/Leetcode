78dc1d5d22dec5ecd6f60e549c05cd56437b8ab1
Naved Sohail <65990919+naved232doom@users.noreply.github.com>
2022-08-08 08:32:39 +0530

    Time: 6 ms (27.16%), Space: 6.8 MB (30.10%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    typedef unsigned long int ll;
+    typedef unsigned long long ll;
     int combinationSum4(vector<int>& nums, int x) {
         int n=(int)(nums.size());
         
         sort(nums.begin(),nums.end());
-        const int N=*max_element(nums.begin(),nums.end())+1;
+        //const int N=*max_element(nums.begin(),nums.end())+1;
         vector<ll> dp(x+1,0);
        dp[0]=1;
         for(int s=0;s<=x;++s){
