738df6750397999fb6876399c131924c99c90810
Naved Sohail <65990919+naved232doom@users.noreply.github.com>
2022-08-09 18:10:39 +0530

    Time: 94 ms (68.00%), Space: 9.5 MB (50.15%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
@@ -3,31 +3,35 @@ public:
     typedef long long ll;
     int numFactoredBinaryTrees(vector<int>& arr) {
         int n=(int)(arr.size());
-        map<ll,ll> dp;
+        vector<ll> dp(n,1);
+        map<int,int> idx;
         sort(arr.begin(),arr.end());
         ll ans=0;
+        for(int i=0;i<n;++i){
+            idx[arr[i]]=i;
+        }
         const int mod=1e9+7;
         for(int i=0;i<n;++i){
-            dp[arr[i]]=1;
+            dp[i]=1;
         }
         for(int i=0;i<n;++i){
             for(int j=0;j<=i;++j){
                 int s=arr[j];
-                if(arr[i]%s!=0) continue;
+                if(arr[i]%s!=0 || idx.find(arr[i]/s)==idx.end()) continue;
                 ll ans_here=0;
                 // cout<<"arr[i] "<<arr[i]<<endl;
                 // cout<<"s "<<s<<endl;
                 // cout<<"dp[s] "<<dp[s]<<endl;
                 // cout<<"dp[arr[i]/s] "<<dp[arr[i]/s]<<endl;
-                ans_here=(dp[s]%mod*dp[arr[i]/s]%mod)%mod;
+                ans_here=(dp[j]%mod*dp[idx[arr[i]/s]]%mod)%mod;
                 //ans_here=max(ans_here,1ll);
-                dp[arr[i]]+=ans_here;
-                dp[arr[i]]%=mod;
+                dp[i]+=ans_here;
+                dp[i]%=mod;
                 
             }
         }
         for(auto &it:dp){
-            ans+=it.second;
+            ans+=it;
            // cout<<it.first<<" "<<it.second<<endl;
             ans%=mod;
         }
