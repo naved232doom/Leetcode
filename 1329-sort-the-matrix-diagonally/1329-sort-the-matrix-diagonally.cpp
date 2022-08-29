ffa942d63b3570feea6fdd5e4becc218997b5361
Naved Sohail <65990919+naved232doom@users.noreply.github.com>
2022-08-29 07:16:06 +0530

    Time: 20 ms (43.88%), Space: 9.6 MB (10.63%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
-        
         int n=(int)(mat.size());
         int m=(int)(mat[0].size());
-        vector<vector<int>> vis(n,vector<int>(m));
+        map<int,priority_queue<int,vector<int>,greater<int>>> mp;
         for(int i=0;i<n;++i){
             for(int j=0;j<m;++j){
-                if(vis[i][j]) continue;
-                vector<int> pos;
-                int x=i,y=j;
-                for(;x>=0&&x<n&&y>=0&&y<m;x++,++y){
-                    pos.push_back(mat[x][y]);
-                    vis[x][y]=1;
-                }
-                sort(pos.begin(),pos.end());
-                x=i,y=j;
-                for(int it=0;x>=0&&x<n&&y>=0&&y<m;x++,++y,++it){
-                    mat[x][y]=pos[it];
-                }
+                mp[i-j].push(mat[i][j]);
+            }
+        }
+        for(int i=0;i<n;++i){
+            for(int j=0;j<m;++j){
+                mat[i][j]=mp[i-j].top();
+                mp[i-j].pop();
             }
         }
         return mat;
